bdc12721fd3dc955d2eaa674d30ada3f16cc8f52
Marcin Łukasik <marcin@eims.pl>
2021-02-20 20:09:59 +0100

    controler card v.3.0

--- src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
@@ -1,7 +1,7 @@
 package com.kodilla.ecommercee.repository;
 
-import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.domain.Cart;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CartRepository extends CrudRepository<Product, Long> {
+public interface CartRepository extends CrudRepository<Cart, Long> {
 }
